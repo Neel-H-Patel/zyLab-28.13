d97acaf0edd50f4b0bb7cf42328e7a04e8bc70cb
zachhilbert2005 <167340927+zachhilbert2005@users.noreply.github.com>
2024-04-19 11:36:09 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -4,6 +4,22 @@
 #include <string>
 using namespace std;
 
-/* Type your code here */
+class ItemToPurchase { 
+public:
+   ItemToPurchase();
+   void SetName(string name);
+   string GetName();
+   
+   void SetPrice(int price);
+   int GetPrice();
+   
+   void SetQuantity(int quantity);
+   int GetQuantity();
+
+private:
+   string itemName = "none";
+   int itemPrice = 0;
+   int itemQuantity = 0;
+};
 
 #endif
